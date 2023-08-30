86082106a6f69b3b9f77820dc133c0e9fc5f5dcf
Mike V <mike@iglab.dev>
2023-08-30 12:04:46 -0400

    added comments

--- colourgame.py
+++ colourgame.py
@@ -16,34 +16,41 @@ numberWrong = 0
 
 
 while gameover == False:
-    # get user input
     print('______________________________\n\n')
-    
     print('You have guessed ' + str(numberRight) + ' right and ' + str(numberWrong) + ' wrong.')
+    
+    # get user input
     userinput = input('Enter a color (RGB) or "exit" to quit : ')
     if userinput.lower() == 'exit':
         gameover = True
         break
+      
     # get random color
     randomcolor = random_color()
     
+    # convert user input to full color name if letter is used
     if userinput.lower() == 'r':
         userinput = 'red'
     elif userinput.lower() == 'g':
         userinput = 'green'
     elif userinput.lower() == 'b':
         userinput = 'blue'
+        
     # check if input is ok
     if (userinput.lower() in colors):
         print('You entered: ' + userinput.lower())
+        # input is ok, so check if it is right
         if(userinput.lower() == colors[randomcolor]):
+          # handle the right guess
             print('You guessed right!')
             numberRight += 1
-            # handle the right guess
+            
         else:
+            # handle the wrong guess
             print('You guessed wrong, the color was: ' + colors[randomcolor])
             numberWrong += 1
     else:
-        print('That is not a valid colour, try again.')
+        #input if not ok
+        print(userinput + ' is not a valid colour, try again.')
         
     time.sleep(2)
